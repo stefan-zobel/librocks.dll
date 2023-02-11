28814aa5c0a7c663654948deabf2aa202acb4d74
stefan-zobel <stefan-zobel@users.noreply.github.com>
2023-02-11 23:34:02 +0100

    extend RocksException from std::runtime_error

--- include/client/RocksException.h
+++ include/client/RocksException.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <string>
+#include <stdexcept>
 
-class RocksException {
+class RocksException : public std::runtime_error {
 
 public:
-    RocksException() = default;
+    RocksException() : std::runtime_error("") {}
     virtual ~RocksException() = default;
 
     explicit RocksException(char* message);
@@ -14,11 +15,6 @@ public:
     explicit RocksException(const std::string& message);
     explicit RocksException(std::string& message);
     RocksException(const char* file, long line, std::string& message);
-
-    const char* what() const noexcept;
-
-private:
-    std::string msg;
 
 private:
     static std::string l2s(long value);
--- src/client/RocksException.cpp
+++ src/client/RocksException.cpp
@@ -2,36 +2,34 @@
 #include "client/RocksException.h"
 
 
-RocksException::RocksException(char* message) {
-    msg.append(message).append("\n");
+RocksException::RocksException(char* message) 
+    : std::runtime_error(std::string(message).append("\n")) {
 }
 
-RocksException::RocksException(const char* message) {
-    msg.append(message).append("\n");
+RocksException::RocksException(const char* message) 
+    : std::runtime_error(std::string(message).append("\n")) {
 }
 
-RocksException::RocksException(const char* file, long line, const char* message) {
-    msg.append(file).append(":").append(l2s(line)).append(": ").append(message).append("\n");
+RocksException::RocksException(const char* file, long line, const char* message) 
+    : std::runtime_error(std::string(file).append(":").append(l2s(line)).append(": ")
+        .append(message).append("\n")) {
 }
 
-RocksException::RocksException(const std::string& message) {
-    msg.append(message).append("\n");
+RocksException::RocksException(const std::string& message) 
+    : std::runtime_error(std::string(message).append("\n")) {
 }
 
-RocksException::RocksException(std::string& message) {
-    msg.append(message).append("\n");
+RocksException::RocksException(std::string& message) 
+    : std::runtime_error(std::string(message).append("\n")) {
 }
 
-RocksException::RocksException(const char* file, long line, std::string& message) {
-    msg.append(file).append(":").append(l2s(line)).append(": ").append(message).append("\n");
+RocksException::RocksException(const char* file, long line, std::string& message) 
+    : std::runtime_error(std::string(file).append(":").append(l2s(line)).append(": ")
+        .append(message).append("\n")) {
 }
 
 std::string RocksException::l2s(long value) {
     char buffer[64] = { 0 };
     sprintf_s(buffer, 64, "%ld", value);
     return std::string(buffer);
-}
-
-const char* RocksException::what() const noexcept {
-    return msg.c_str();
 }
