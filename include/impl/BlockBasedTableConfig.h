0ae39872817de729da139269f799d25943465eea
stefan-zobel <stefan-zobel@users.noreply.github.com>
2026-02-04 16:58:39 +0100

    use format_version = 6

--- include/impl/BlockBasedTableConfig.h
+++ include/impl/BlockBasedTableConfig.h
@@ -8,6 +8,7 @@ public:
     static inline const rocksdb::BlockBasedTableOptions options = [] {
         rocksdb::BlockBasedTableOptions opts;
         opts.block_size = 16 * 1024;
+        opts.format_version = 6;
         return opts;
         }();
 };
